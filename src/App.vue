af4356bf0abe8dbb86a6ca5d4ded2c879ae36a19
Willi Berger <willi.berger@gmx.at>
2020-09-05 14:13:03 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
+  <div id="app" class="small-container">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="Welcome Willi to Your Vue.js App"/>
+    <h1>Employees</h1>
+
+    <employee-form  @add:employee="addEmployee"/>
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable'
+import EmployeeForm from './components/EmployeeForm'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    EmployeeTable,
+    EmployeeForm
+  },
+
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
+
+  methods: {
+    addEmployee: function(employee) {
+      console.log("addEmpoyee");
+      this.employees = [...this.employees, employee]
+    }
   }
+
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="my-component">
+        <h1>Hello, this is my component</h1></div>
+</template>
+<script>
+export default {
+    
+    name:'my-component',
+}
+</script>
+
+<style scoped>
+
+</style>
